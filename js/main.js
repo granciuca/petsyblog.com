54c1a92366141637b7553d34bf065c2fb0c1868b
Adrian Granciuc <granciuca@gmail.com>
2023-08-11 15:43:23 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+'use strict'
+
+// const headerLogo = document.querySelector('.header-logo-change');
+
+
+
+
+window.addEventListener('load', (event) => {
+    const headerLogo = document.querySelector('.header-logo-img');
+    const logoSrc = 'https://electromag.md/wp-content/uploads/2022/10/unnamed.gif';
+    headerLogo.src = logoSrc;
+});
